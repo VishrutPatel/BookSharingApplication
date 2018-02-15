5c9164ed3395590d5af28432b54d765bacbdc921
princemandlewala <prince001mandlewala@gmail.com>
2018-02-15 10:53:35 -0500

    Git Test

--- BookSharingApplication/models.py
+++ BookSharingApplication/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class User(models.Model):
     email_Id = models.EmailField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     addr1 = models.CharField(max_length=255)
     addr2 = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
