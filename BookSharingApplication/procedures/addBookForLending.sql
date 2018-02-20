69f35aff49ae242b5fb0b371ee867e1f54a6f7ce
VishrutPatel <patelvishrut9395@gmail.com>
2018-02-20 13:59:21 -0500

    SQL Procedure for book lending

---
+++ BookSharingApplication/procedures/addBookForLending.sql
@@ -0,0 +1,20 @@
+delimiter //
+
+CREATE PROCEDURE `addBookForLending`(
+IN inptemail VARCHAR(50),
+IN inpttitle VARCHAR(30),
+IN inptgenre VARCHAR(100),
+IN inptauthor VARCHAR(100),
+IN inptstartdatetime DATETIME,
+IN inptenddatetime DATETIME
+ )
+ 
+ 
+BEGIN
+      
+      insert into book 
+      (email,title,author,genre,start_Date_Time,end_Date_Time)
+      values 
+      (inptemail,inpttitle,inptauthor,inptauthor,inptgenre,inptstartdatetime,inptenddatetime);
+      
+END//
