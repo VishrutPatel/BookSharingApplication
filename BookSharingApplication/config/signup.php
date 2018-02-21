fe82822a7a51965d8b9c0c53eb70d29546a367d9
princemandlewala <prince001mandlewala@gmail.com>
2018-02-20 22:58:35 -0500

    changed signup form for checking whether the user exists or not

--- BookSharingApplication/config/signup.php
+++ BookSharingApplication/config/signup.php
@@ -1,6 +1,7 @@
 <?php
 
 //Connect to Database
+require (__DIR__."/Database.php");
 $dbconn = new Database();
 $conn = $dbconn->getConnection();
 
@@ -11,17 +12,22 @@ else {
     //Execute a procedure call to validate signup (Check for already present users)
     //get a json file and decode it
     $json = file_get_contents("php://input");
-    $data = json_decode($json);
-    $user="";
-    $stmt=$conn->query("CALL ValidateSignUp('".$user."',@p1);");
-    $stmt2=$conn->query("SELECT @p1 FROM VALIDATE");
+    $data = json_decode($json,true);
+    $user=$data['email'];
+    $stmt=$conn->query("CALL validateSignUp('".$user."',@p1);");
+    $stmt2=$conn->query("SELECT @p1 AS VALIDATE");
     //The procedure returns only a bool value.
     $result = $stmt2->fetch();
-    if ($result == true) {
+    if ($result['VALIDATE'] == 1) {
         //return a true value to the front-end for a go ahead to signup
+        echo json_encode(
+            array("message"=>"Found")
+        );
     } else {
         //return false in case an email-id already exists in the database
-
+        echo json_encode(
+            array("message" => "Not Found")
+        );
     }
 }
 
