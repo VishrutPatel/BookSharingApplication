53a661d670caa8c5103ba9569c6859e8f040d1ff
VishrutPatel <patelvishrut9395@gmail.com>
2018-03-10 15:26:08 -0500

    incorrect changes stacked

--- BookSharingApplication/procedures/addBorrowRequest.sql
+++ BookSharingApplication/procedures/addBorrowRequest.sql
@@ -10,14 +10,10 @@ OUT check INT
  
  
 BEGIN
-      select max(br.id) as val
-      from borrowrequest as br;
+      
       insert into borrowrequest 
       (book_Id,borrower_email)
       values 
       (inptborroweremail,inptbookid);
-      select val as "1"
-      from borrowrequest
-      where id = val+1;
 
 END//
