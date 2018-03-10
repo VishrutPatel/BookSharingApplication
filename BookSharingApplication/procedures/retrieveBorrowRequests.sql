39fb94bc1e161b3facf58ed199ae82ca2b10987e
VishrutPatel <patelvishrut9395@gmail.com>
2018-03-10 18:31:20 -0500

    retrieve Borrow request Update

--- BookSharingApplication/procedures/retrieveBorrowRequests.sql
+++ BookSharingApplication/procedures/retrieveBorrowRequests.sql
@@ -3,13 +3,15 @@ delimiter //
 CREATE PROCEDURE `retrieveBorrowRequests`(
 IN inptemail varchar(50),
 OUT otptborroweremail varchar(50),
-OUT otptratings FLOAT(3,2)
- )
+OUT otptratings FLOAT(3,2),
+OUT otptbookID INT,
+OUT otptTitle varchar(50),
+)
  
  
 BEGIN
       
-      SELECT b.email,b.ratings,count(*)
+      SELECT br.book_Id,b.email,b.ratings,bk.title
       FROM borrower b, borrowrequest br, book bk
       WHERE b.email = br.borroweremail AND bk.id = br.book_Id AND bk.email = inptemail
       
